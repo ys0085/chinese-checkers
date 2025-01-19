610b490b348ce4aacf5bc8db8048004ad2ca853e
khhhalin <162209922+khhhalin@users.noreply.github.com>
2025-01-19 20:37:22 +0100

    merger

--- common/src/main/java/com/tp/Move.java
+++ common/src/main/java/com/tp/Move.java
@@ -1,11 +1,14 @@
 package com.tp;
 
 public class Move {
-    final public int x1, y1, x2, y2;
-    Move(int x1, int y1, int x2, int y2){
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+    final public Position from, to;
+    Move(){
+        this.from = new Position();
+        this.to = new Position();
+    }
+    Move(Position from, Position to){
+        this.from = from;
+        this.to = to;
     }
 }
+
---
+++ common/src/main/java/com/tp/Position.java
@@ -0,0 +1,11 @@
+package com.tp;
+
+public class Position {
+    public int x, y;
+    Position(){};
+    Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }    
+}

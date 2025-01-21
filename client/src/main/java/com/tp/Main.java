2a5622aa219ad6292c55a24a40a79e0a19087593
khhhalin <162209922+khhhalin@users.noreply.github.com>
2025-01-21 22:16:15 +0100

    Update Main.java

--- client/src/main/java/com/tp/Main.java
+++ client/src/main/java/com/tp/Main.java
@@ -17,19 +17,16 @@ public class Main {
             Thread uiThread = new Thread(new UIThread(args));
             uiThread.start();
 
-<<<<<<< Updated upstream
             Client client = Client.getInstance();
             client.setSocket(socket);
             client.setColor(args[0]);
-=======
-            Client client = new Client(socket);
-            client.setColor(args[0].toUpperCase());
->>>>>>> Stashed changes
 
             client.start();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        App.main(args);
     }
 }
